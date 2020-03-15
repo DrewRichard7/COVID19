8b093b7ea3c7f60e72aa880b63f9fb07380b3d13
aclayrichard <aclayrichard@clayrichard.attlocal.net>
2020-03-15 18:18:25 -0400

    tried to open files from 01/22 to today but didnt quite get there

--- COVIDMAT.m
+++ COVIDMAT.m
@@ -1,14 +1,39 @@
 close all;clc
-format long g
 time = clock;
-month = sprintf('%02d',time(2));
-day = time(3)-2;
-year = time(1);
+currentmonth = fprintf('%02d',time(2))
+currentday = time(3)-1;
+currentyear = time(1);
 
-currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
+for year = 2020:currentyear
+    for month = 01:currentmonth
+        disp(month)
+%         if month == 04 || month == 06 || month == 09 || month == 11
+%            for day = 01:30
+%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
+%            end 
+%         elseif month == 02
+%            for day = 01:29
+%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
+%            end
+%         elseif month == 01
+%            for day = 22:31
+%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
+%            end
+%         else 
+%            for day = 01:31
+%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
+%            end
+%         if month == currentmonth
+%            for day = 01:currentday
+%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
+%            end
+%         end
+        
 
 % readmatrix uses just the numbers whereas importdata gives two files with
 % numbers in a matrix (.data) and then names as a text file (.textdata)
 % currentdata = readmatrix(strcat('/Users/aclayrichard/Desktop/lab/COVID19/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/0',string(month),'-',string(day),'-',string(year),'.csv'))
-
-CasesinUSA = importdata(strcat('COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series');
+   
+        % end 
+    end
+end 
