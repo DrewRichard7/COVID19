9b35177237b492c27b41b0b1064cf8747a867924
Drew Richard <DrewRichard@drewrichard.local>
2020-03-15 11:56:30 -0500

    changed to relative path

--- COVIDMAT.m
+++ COVIDMAT.m
@@ -5,7 +5,7 @@ month = time(2);
 day = time(3)-2;
 year = time(1);
 
-currentdata = importdata(strcat('/Users/aclayrichard/Desktop/lab/COVID19/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/0',string(month),'-',string(day),'-',string(year),'.csv'));
+currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/0',string(month),'-',string(day),'-',string(year),'.csv'));
 
 % readmatrix uses just the numbers whereas importdata gives two files with
 % numbers in a matrix (.data) and then names as a text file (.textdata)
