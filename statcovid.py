8fa7f05573ae28d5d27749e4353a315fe258f3cb
Drew Richard <DrewRichard@drewrichard.local>
2020-03-14 19:54:38 -0500

    changed statcovid.py to relative path search

--- statcovid.py
+++ statcovid.py
@@ -5,8 +5,13 @@ import pandas as pd
 from matplotlib.widgets import MultiCursor
 import statistics
 import scipy.stats 
-df = pd.read_csv(
-    '/Users/DrewRichard/Desktop/Code/Python/covid19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv', index_col=0)
+try:
+    df = pd.read_csv(
+        'COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv', index_col=0)
+except:
+    print("Dataset not available, please clone repo listed in README")
+    exit()
+
 df.set_index("Country/Region", inplace=True)
 dates = df.iloc[42, 3:46]
 print(dates)
