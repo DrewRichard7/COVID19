e39e9d5427606cc63233ad2720f09a82cb93139c
Clay Richard <aclayrichard@clayrichard.attlocal.net>
2020-03-15 12:53:20 -0400

    updated .m files with reading data

---
+++ COVIDMAT.m
@@ -0,0 +1,12 @@
+close all;clc
+format long g
+time = clock;
+month = time(2);
+day = time(3)-2;
+year = time(1);
+
+currentdata = importdata(strcat('/Users/aclayrichard/Desktop/lab/COVID19/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/0',string(month),'-',string(day),'-',string(year),'.csv'));
+
+% readmatrix uses just the numbers whereas importdata gives two files with
+% numbers in a matrix (.data) and then names as a text file (.textdata)
+% currentdata = readmatrix(strcat('/Users/aclayrichard/Desktop/lab/COVID19/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/0',string(month),'-',string(day),'-',string(year),'.csv'))
