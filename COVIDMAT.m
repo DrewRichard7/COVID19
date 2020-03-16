847abcd17ef99fc36c588283b11d20013c3159f3
aclayrichard <aclayrichard@clayrichard.attlocal.net>
2020-03-16 18:09:25 -0400

    trying to read the updated .csv files but not working

--- COVIDMAT.m
+++ COVIDMAT.m
@@ -1,39 +1,26 @@
-close all;clc
+close all;clc;clear all
+
 time = clock;
-currentmonth = fprintf('%02d',time(2))
-currentday = time(3)-1;
-currentyear = time(1);
+currentmonth = sprintf('%02d',time(2));
+currentday = num2str(time(3)-2);
+currentyear = num2str(time(1));
 
-for year = 2020:currentyear
-    for month = 01:currentmonth
-        disp(month)
-%         if month == 04 || month == 06 || month == 09 || month == 11
-%            for day = 01:30
-%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
-%            end 
-%         elseif month == 02
-%            for day = 01:29
-%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
-%            end
-%         elseif month == 01
-%            for day = 22:31
-%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
-%            end
-%         else 
-%            for day = 01:31
-%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
-%            end
-%         if month == currentmonth
-%            for day = 01:currentday
-%                currentdata = importdata(strcat('COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/',string(month),'-',string(day),'-',string(year),'.csv'));
-%            end
-%         end
+currentdata = readmatrix('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv');
+
+% countUS = count(currentdata.textdata,'US');
+% amountofUS = sum(countUS);
+
+% if currentdata.textdata(:,2) == string(US)
+%    for i = 1:amountofUS(2)
+%        confirmedcases = sum(currentdata.data(,:
+%    end
+%    
+% end
+% a = 0
+% for i = 1:amountofUS
+%     if countUS == 1
+%         a = a + currentdata.data(countUS(
         
-
-% readmatrix uses just the numbers whereas importdata gives two files with
-% numbers in a matrix (.data) and then names as a text file (.textdata)
-% currentdata = readmatrix(strcat('/Users/aclayrichard/Desktop/lab/COVID19/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/0',string(month),'-',string(day),'-',string(year),'.csv'))
-   
-        % end 
-    end
-end 
+        
+        
+        
