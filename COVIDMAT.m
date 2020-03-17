1d00706cabd05d152e4492c95b979908aee0d845
aclayrichard <aclayrichard@clayrichard.attlocal.net>
2020-03-17 12:22:15 -0500

    found a way to read table

--- COVIDMAT.m
+++ COVIDMAT.m
@@ -5,7 +5,16 @@ currentmonth = sprintf('%02d',time(2));
 currentday = num2str(time(3)-2);
 currentyear = num2str(time(1));
 
-currentdata = readmatrix('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv');
+currentdata = readtable('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv');
+
+
+
+% cat(currentdata)
+
+% [currentdata.textdata,currentdata.data]
+
+% globalconfirmed = nansum(currentdata); % columns 5-length of array
+
 
 % countUS = count(currentdata.textdata,'US');
 % amountofUS = sum(countUS);
