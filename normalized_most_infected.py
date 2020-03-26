28de39d2fba4302e86f4968cb55c8e81b47e2b1e
Drew Richard <DrewRichard@drewrichard.local>
2020-03-25 23:37:25 -0500

    Normalized based on most infected countries

--- normalized_most_infected.py
+++ normalized_most_infected.py
@@ -1,63 +1,69 @@
 
-# VERY MESSED UP, NEEDS READDRESSING
-
-
+# If it's been a while since this was run, uncomment the read_html & to_csv
+# lines to import the latest data. Don't forget to comment the read_csv before
+# you do.
+# Make sure to recomment after import to avoid repetitive pinging of
+# website's server.
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
 url1 = 'https://www.worldometers.info/world-population/population-by-country/'
 
-# if it's been a while since this was run, uncomment the read_html & to_csv
-# lines to import the latest data. Don't forget to comment the read_csv before
-#  you do.
-# make sure to recomment after import
+# Import table of countries and their total population
 try:
     # population_list = pd.read_html(url1, attrs={'class': 'table'})
     # population_list[0].to_csv('populations.csv')
-    countries_population_df = pd.read_csv('populations.csv')
+    population_df = pd.read_csv('populations.csv')
 except IOError as e:
     print(e)
     exit()
 
 url2 = 'https://www.worldometers.info/coronavirus/#countries'
+# Import table of countries and their population infected with COVID-19
 try:
     # pop_list = pd.read_html(url2, attrs={'class': 'table'})
     # pop_list[0].to_csv('infected_countries_pop.csv')
-    countries_infected_list = pd.read_csv('infected_countries_pop.csv')
+    infected_df = pd.read_csv('infected_countries_pop.csv')
 except IOError as e:
     print(e)
     exit()
-print(countries_infected_list.head())
-infected_df = infected_list
-dictionary = {'Country': infected_df['Country,Other'].iloc[0:10]}
-topten_infected_df = pd.DataFrame(dictionary)
+
+# Create a dataframe of the top ten most infected countries
+topten_infected_df = pd.DataFrame(
+    {'Country': infected_df['Country,Other'].iloc[0:10]})
 topten_infected_df['Infections'] = infected_df['TotalCases'].iloc[0:10]
-infected_df = pd.DataFrame()
+
+# Here I had to change the nameprints in the csv files because one had UK
+# and the other had United Kingdom, but for USA and S. Korea also.
+# My skills aren't good enough yet to find and change that in code,
+# so I did it manually.
+big_df = pd.DataFrame()
 for row in topten_infected_df.itertuples():
-    infected_df = pd.concat([infected_df,
-                            population_df[population_df['Country\
- (or dependency)']
-                             == row[1]]])
-cols = ['Yearly Change', 'Net Change', 'Density (P/Km²)',
+    big_df = pd.concat([big_df,
+                        population_df[population_df['Country \
+(or dependency)']
+                                      == row[1]]])
+cols = ['#', 'Yearly Change', 'Net Change', 'Density (P/Km²)',
         'Land Area (Km²)', 'Migrants (net)', 'Fert. Rate',
         'Med. Age', 'Urban Pop %', 'World Share']
-infected_df = infected_df.drop(columns=cols)
-infected_df['end'] = infected_df.iloc[:, 3]
-inf_pop_df = pd.DataFrame()
-inf_pop_df['Country (or dependency)'] = infected_df['Country (or dependency)']
-inf_pop_df['Population'] = infected_df['end']
-inf_pop_df = inf_pop_df.groupby(['Country (or dependency)']).agg('sum')
+big_df = big_df.drop(columns=cols)
+big_df = big_df.loc[:, ~big_df.columns.str.contains('^Unnamed')]
+big_df = big_df.reset_index(drop=True)
+big_df.rename(columns={'Country (or dependency)': 'Country',
+                       'Population (2020)': 'Total Population'}, inplace=True)
+big_df['Infected Population'] = topten_infected_df['Infections']
 
-merged = pd.merge(topten_infected_df, inf_pop_df, on='Country (or dependency)',
-                  suffixes=[' Total', ' Infected'])
-merged['Normalized Percentage'] = (merged['Population Infected'] /
-                                   merged['Population Total'])*100
-merged = merged.drop(columns=['Population Total', 'Population Infected'])
+# Here I added a column with the infections divided by population
+# to normalize the data.
+big_df['Percent Infected'] = (big_df['Infected Population'] /
+                              big_df['Total Population'])*100
 
+# Plot the data on a bar chart to show direct comparisons
 fig = plt.figure()
 plt.xlabel('Country')
-plt.ylabel('Normalized % Pop. Infected')
-plt.title('Normalized Infections of Top Ten Most Populous Countries')
-plt.bar(merged['Country'], merged['Normalized Percentage'])
+plt.ylabel('Population Infected (%)')
+plt.title('Normalized Infections of Top Ten Most Infected Countries')
+plt.xticks(rotation=30)
+plt.bar(big_df['Country'], big_df['Percent Infected'])
 plt.show()
